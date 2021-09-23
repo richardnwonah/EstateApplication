fa895f8d24b884ec1cd5421b4c704f9cc304a7fa
Richard nwonah <nwonahr@gmail.com>
2021-09-23 09:54:40 +0100

    added authentication DbContext

--- src/EstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/EstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,15 @@
+using EstateApp.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace EstateApp.Data.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        :base (options)
+        {
+
+        }
     }
 }
